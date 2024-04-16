e02c1be015cb91ef1fff0c2c7133c2b352c74584
Tor Kay <65842590+torkay@users.noreply.github.com>
2024-04-16 13:40:41 +1000

    TODO note list

--- vendor_storage.py
+++ vendor_storage.py
@@ -1,2 +1,4 @@
+# TODO: Have vendors saved as json and read via func here.
+
 manheim = {"Name": "Manheim", "Logo": "https://4vector.com/i/free-vector-manheim-auctions_033937_manheim-auctions.png", "Url": "https://manheim.com.au/damaged-vehicles/search"}
 pickles = {"Name": "Pickles", "Logo": "https://pbs.twimg.com/profile_images/952744725617852416/mTFTUM4p_400x400.jpg", "Url": "https://www.pickles.com.au/damaged-salvage/item/search#!/search-result"}
